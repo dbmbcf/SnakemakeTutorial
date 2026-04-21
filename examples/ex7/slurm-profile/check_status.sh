3a8a2b528aba8685b091a31f9422c1d0fb6de2fe
Florian Geier <florian.geier@unibas.ch>
2026-04-21 13:06:42 +0200

    Added slurm example

--- examples/ex7/slurm-profile/check_status.sh
+++ examples/ex7/slurm-profile/check_status.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# Check status of Slurm job
+# Check status of slurm job
 
 jobid="$1"
+
+if [[ "$jobid" == Submitted ]]
+then
+  echo Invalid job ID: "$jobid" >&2
+  exit 1
+fi
 
 output=`sacct -j "$jobid" --format State --noheader | head -n 1 | awk '{print $1}'`
 
--- examples/ex7/slurm-profile/submit.py
+++ examples/ex7/slurm-profile/submit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-import os, sys
+import os, sys, getpass, pwd, grp
+
 from snakemake.utils import read_job_properties
+from snakemake.common.configfile import load_configfile
 
 jobscript = sys.argv[1]
 job_properties = read_job_properties(jobscript)
 
-## set up scicore defaults
+## set up scicore slurm defaults
 sbatch_options = {
+    'account' : grp.getgrgid(pwd.getpwnam(getpass.getuser()).pw_gid).gr_name,
     'partition' : 'scicore',
+    'qos' : '30min', 
     'mem' : '4G',
-    'time' : 30,
     'output' : 'log/{rule}/slurm.%j.%N.out'.format(rule=job_properties["rule"]),
     'job-name' : 'smk.{rule}'.format(rule=job_properties["rule"])
 }
@@ -20,6 +23,10 @@ for key in sbatch_options.keys():
     if key in res.keys():
         sbatch_options[key] = res[key]
 
+## adjust memory
+if "mem_mb" in res.keys():
+    sbatch_options["mem"] = res["mem_mb"]
+    
 ## add qos depending on time
 def time2qos(time_min):
     if time_min <= 30:
@@ -32,9 +39,13 @@ def time2qos(time_min):
         qos = "1week"
     return(qos)
 
-sbatch_options["qos"] = time2qos(sbatch_options["time"])
-
-## add threads
+## check in this order:
+for key in ["time","time_min","runtime"]:
+    if key in res.keys():
+        sbatch_options["qos"] = time2qos(res[key])
+        break
+    
+## adjust threads
 sbatch_options["cpus-per-task"] = job_properties["threads"] if "threads" in job_properties.keys() else 1
 
 ## create log dir
@@ -45,9 +56,7 @@ if not os.path.exists(out):
 ## convert to string
 options = ""
 for k, v in sbatch_options.items():
-    val = ""
-    if v is not None:
-        val = f"={v}"
+    val = "" if v is None else f"={v}"
     options += f"--{k}{val} "
-        
+
 os.system("sbatch --parsable {options} {script}".format(options=options, script=jobscript))
